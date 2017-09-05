39a6a232d7d489d98d2542cbd7943a7c684cae0d
Jason Poon <git@jasonpoon.ca>
2017-09-04 20:51:43 -0700

    docs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ServerInfo
+
+A simple Windows container that prints the ServerInfo.
+
+## Usage
+
+Install [Docker for Windows](https://docs.docker.com/docker-for-windows/) switching the daemon to *Windows Containers*. In Powershell,
+
+```
+$ ./build.ps1
+$ docker run --name server-info server-info
+$ docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" server-info
+
--- build.ps1
+++ build.ps1
@@ -1,11 +1,12 @@
+# Build and Publish to Folder
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 $options = "/p:DeployOnBuild=true /p:PublishProfile='FolderProfile' /p:VisualStudioVersion=14.0 /p:AspnetMergePath='C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools'"
-$apiproj = "ServerInfo/ServerInfo.csproj"
+$apiproj = "ServerInfo.sln"
 
-# Publish the WebApi to Server...
 $build = $msbuild + " $apiproj " + $options
 Invoke-Expression $build
 
+# Docker Build
 $tag = "server-info"
 $siteroot = ".\ServerInfo\bin\Release\PublishOutput"
 $siteroot
